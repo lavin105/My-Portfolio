4ce191d704fa8377a7f2ad03f53588626f52eecf
lavin105 <lavin105@mail.chapman.edu>
2018-07-18 20:30:18 -0700

    added about me info and updated icon

--- my-portfolio/src/Components/About.js
+++ my-portfolio/src/Components/About.js
@@ -5,7 +5,7 @@ import Dots from "./Images/dots.svg";
 class About extends Component {
   render() {
     return (
-      <div id='page_wrapper'>
+      <div id="page_wrapper">
         <div id="main_image" />
         <div id="image_holder" />
         <br />
@@ -24,45 +24,31 @@ class About extends Component {
         <img id="dots1" src={Dots} alt="" />
         <div id="main_summary1">
           <h2 id="aboutme">A little about me</h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-          quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
-          mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
-          Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad
-          litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
-          sodales ligula in libero. Sed dignissim lacinia nunc.
+          My name is Brandon Lavinsky and I am currently pursuing my Bachelors
+          of Science in Software Engineering as well as a Minor in Data
+          Analytics at Chapman University . I thoroughly enjoy web development
+          because it allows me to be extra creative with visuals and bring out
+          my artistic nature. However I not only develop web applications but
+          also have experience with other programming languages such as Java,
+          C#, Python, and more. In my spare time aside from school and work I
+          enjoy surfing, skateboard, playing basketball, and pretty much any
+          outdoor activity.
           <br />
           <br />
-          Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem
-          at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut
-          ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel,
-          suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia
-          aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt
-          sed, euismod in, nibh. Quisque volutpat condimentum velit.
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
+        
+        
 
         <img id="dots2" src={Dots} alt="" />
         <div id="main_summary2">
           <h2 id="aboutme">Technical Skills</h2>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-          quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
-          mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
-          Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad
-          litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
-          sodales ligula in libero. Sed dignissim lacinia nunc.
+          I have had a lot of exposure to many different programming languages
+          and technologies throughout my schooling. I have worked with Python,
+          Java, C++, C#, MySql, JDBC, and UML. I had to learn web development on
+          my own due to the fact that no courses were offered regarding web
+          development at my University.
           <br />
           <br />
-          Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem
-          at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut
-          ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel,
-          suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia
-          aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt
-          sed, euismod in, nibh. Quisque volutpat condimentum velit.
           <br />
           <br />
           <br />
